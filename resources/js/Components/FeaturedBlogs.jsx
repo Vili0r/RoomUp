38e460e580d5ce4fa9d9a11768d29d52ff9a20b4
Vili0r <107792988+Vili0r@users.noreply.github.com>
2023-12-19 19:55:47 +0000

    Changes for deployment 10

--- resources/js/Components/FeaturedBlogs.jsx
+++ resources/js/Components/FeaturedBlogs.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
-import { FeaturedBlogCard } from ".";
+import FeaturedBlogCard from "./FeaturedBlogCard";
 
 const responsive = {
     superLargeDesktop: {
